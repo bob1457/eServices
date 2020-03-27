039e2c1be5cae33e8660be290f42bda3ff031664
Bob Yuan <bob.yuan@yahoo.com>
2020-03-26 17:18:32 -0700

    Add Identity Service and test Api project,  and tested with client credentials authenticaiton

---
+++ src/eServices.IdentityService/Config.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eServices.IdentityService
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("ResurceApi", "Test Api Resource")
+            };
+        }
+
+        public static IEnumerable<Client> GetAllClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                        "ResurceApi"
+                    }
+                }
+            };
+        }
+    }
+}
