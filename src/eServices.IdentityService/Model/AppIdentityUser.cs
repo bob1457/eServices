cdba94e7d07c52d2c3c2a18c72f82c404114608e
Bob Yuan <bob.yuan@yahoo.com>
2020-03-28 17:27:25 -0700

    Add IdentityServer 4 EntityFramework and integrate with Asp.Net Core Identity setup

--- src/eServices.IdentityService/Model/AppIdentityUser.cs
+++ src/eServices.IdentityService/Model/AppIdentityUser.cs
@@ -8,5 +8,23 @@ namespace eServices.IdentityService.Model
 {
     public class AppIdentityUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserRole { get; set; }
+        public int OrganizationId { get; set; }
+        public bool IsDisabled { get; set; }
+        public string AvatarImgUrl { get; set; }
+        public string Telephone1 { get; set; }
+        public string Telephone2 { get; set; }
+        public string SocialMediaContact1 { get; set; }
+        public string SocialMediaContact2 { get; set; }
+        public string AddressStreet { get; set; }
+        public string AddressCity { get; set; }
+        public string AddressStateProv { get; set; }
+        public string AddressZipPostCode { get; set; }
+        public string AddressCountry { get; set; }
+        public DateTime JoinDate { get; set; }
+        public DateTime LastLogOn { get; set; }
+        public DateTime LastUpdated { get; set; }
     }
 }
--- src/eServices.IdentityService/Startup.cs
+++ src/eServices.IdentityService/Startup.cs
@@ -12,6 +12,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using eServices.IdentityService.Data;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 
 namespace eServices.IdentityService
 {
@@ -27,23 +32,43 @@ namespace eServices.IdentityService
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {             
-             services.AddIdentityServer()
+        {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                                                                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            var connectionString = Configuration.GetSection("ConnectionStrings")["DefaultConnection"]; //@"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-2.0.0;trusted_connection=yes;";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            //var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddIdentity<AppIdentityUser, AppIdentityRole>()
+           .AddEntityFrameworkStores<AppIdentityDbContext>()
+           .AddDefaultTokenProviders();
+
+            services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetAllApiResources())
-                .AddInMemoryClients(Config.GetAllClients())
-                .AddTestUsers(Config.GetUsers())
-                //.AddAspNetIdentity<AppIdentityUser>()
-                
-                //.AddOperationalStore(options =>
-                //{
-                //    options.ConfigureDbContext = b =>
-                //    b.UseSqlServer(Configuration.GetConnectionString("Default"));
-                //    // this enables automatic token cleanup. this is optional.
-                //    options.EnableTokenCleanup = true;
-                //    options.TokenCleanupInterval = 30; // interval in seconds
-                //})
+                //.AddInMemoryIdentityResources(Config.GetIdentityResources())
+                //.AddInMemoryApiResources(Config.GetAllApiResources())
+                //.AddInMemoryClients(Config.GetAllClients())
+                //.AddTestUsers(Config.GetUsers())
+                .AddAspNetIdentity<AppIdentityUser>()
+                // Define Identityserver configuration store containing config data with EF
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                            builder.UseSqlServer(connectionString,
+                                sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                // Define Identityserver operation store containing config data with EF: tokens, consent, etc.
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b =>
+                    b.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                     sql => sql.MigrationsAssembly(migrationsAssembly));
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30; // interval in seconds
+                })
                 ;
 
             services.AddMvc(options =>
@@ -59,6 +84,9 @@ namespace eServices.IdentityService
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // See iinitial data
+            InitializeIdneityServerDatabase(app);
 
             app.UseIdentityServer();
 
@@ -85,6 +113,46 @@ namespace eServices.IdentityService
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private void InitializeIdneityServerDatabase(IApplicationBuilder app)
+        {
+            //throw new NotImplementedException();
+
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetAllClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetAllApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
