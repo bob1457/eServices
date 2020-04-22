6b541c8c34efbd8530531ff2a6912f6979b4c7e3
Bob Yuan <bob.yuan@yahoo.com>
2020-04-22 10:09:15 -0700

    Add IdentityServer4 authentication

---
+++ src/eServices.IdentityServer/Data/Migrations/AspNetIdentity/AspNetIdentityDb/20200421182207_CustomizedUsersIdentityDbMigration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Data.Migrations.AspNetIdentity.AspNetIdentityDb
+{
+    public partial class CustomizedUsersIdentityDbMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "02f5989c-86a9-41d2-b911-f61278743ca2", "AQAAAAEAACcQAAAAEL8qLtUtHTvT+tx+z5OZzbXiNC5zF0hHtfZkshM6Hm6ITFGJyGw5KD8jjnk9IxxaiA==", "bc1a3532-5be2-47b8-93c0-94aa4aedc25a" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "a6e7e6a6-f906-4768-a044-cc9b6a75b7a7", "AQAAAAEAACcQAAAAEL4sjwIaaCYSHaekKV8/MC9URHQ+peoasxwUf7m0CS7TPkBcAgrp+3cJl0hT53URLQ==", "d1026a71-4b44-4a41-972a-744f402651c8" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "b0076419-303f-4939-a76f-6b7f95565c7c", "AQAAAAEAACcQAAAAEMZRDQpPm25luuIw5bYGFZFdomLn8WO6PITE5kO1E5ePhyn8RJcSC5OlgWmBu4xfLA==", "a2078aa0-5c79-4dc1-a4b4-3065a534db8b" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "fa775390-cbc9-456c-a608-524a077dead6", "AQAAAAEAACcQAAAAELnSQMhmsJHJtbH3BxxZEp3MExeSZ6uHnCnpp5oa3mAnV2Jqn4vy+I1O0dUKzoT0Jw==", "6d5df826-fb5e-4193-b30a-dc406ec15b5a" });
+        }
+    }
+}
---
+++ src/eServices.IdentityServer/Startup.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer.Data;
+using IdentityServer.Models;
+using IdentityServer.Services;
+using IdentityServer4.Configuration;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            //var builder = services.AddIdentityServer()
+            //    .AddInMemoryIdentityResources(Config.Ids)
+            //    .AddInMemoryApiResources(Config.Apis)
+            //    .AddInMemoryClients(Config.Clients);
+
+            services.AddDbContext<ApplicationAuthDbContext>(options => options.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly)));
+            services.AddDbContext<ConfigurationDbContext>(options => options.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly)));
+
+            services.AddIdentity<ApplicationUser,IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+                .AddEntityFrameworkStores<ApplicationAuthDbContext>()
+                .AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.UserInteraction.LoginUrl = "/Account/Login";
+                options.UserInteraction.LogoutUrl = "/Account/Logout";
+                options.Authentication = new AuthenticationOptions()
+                {
+                    CookieLifetime = TimeSpan.FromHours(10), // ID server cookie timeout set to 10 hours
+                    CookieSlidingExpiration = true
+                };
+            })
+            //Identity server configuratin db
+            .AddConfigurationStore(options =>
+             {
+                 options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+             })
+            // Identity server operaatin db
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                options.EnableTokenCleanup = true;
+            })
+            .AddAspNetIdentity<ApplicationUser>();
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+
+            services.AddScoped<IProfileService, ProfileService>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
