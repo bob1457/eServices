f161bfdb371f985efd85664f1e39423b337cdc47
Bob Yuan <bob.yuan@yahoo.com>
2020-04-22 12:36:16 -0700

    Add more porjects

---
+++ src/eServices.Ecommerce.CatelogService/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace eServices.Ecommerce.CatelogService
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
