f33c0c60bf1d3d3529cd02f84c36aa8e05aa16d4
Bob Yuan <bob.yuan@yahoo.com>
2020-04-27 12:22:20 -0700

    Add message bus - part 1

---
+++ src/eServices.Infrastrcutre.MessageBus.Core/Bus/IEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using eServices.Infrastrcutre.Messaging.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eServices.Infrastrcutre.Messaging.Core.Bus
+{
+    public interface IEventHandler
+    {
+        public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+        {
+            Task Handle(TEvent @event);
+        }
+
+        public interface IEventHandler
+        {
+
+        }
+    }
+}
