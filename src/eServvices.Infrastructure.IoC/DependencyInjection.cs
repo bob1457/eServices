5572ac9f318c44736df84fd434480cee5afd0da1
Bob Yuan <bob.yuan@yahoo.com>
2020-04-27 15:48:30 -0700

    Completed message bus implementation

---
+++ src/eServvices.Infrastructure.IoC/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using eServices.Infrastrcutre.Messaging.Core.Bus;
+using eServices.Infrastructure.MessageBus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eServvices.Infrastructure.IoC
+{
+    public class DependencyInjection
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
